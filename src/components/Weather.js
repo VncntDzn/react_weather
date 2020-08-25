ff6df9cfd51e77638a42f1b2af8914265284fc00
Vincent <48564305+VncntDzn@users.noreply.github.com>
2020-08-25 20:45:22 +0800

    change http to https

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -12,7 +12,7 @@ export default function Weather() {
 
 
     const retrieveWeather = async () => {
-        const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=71655ad9b62c3bd8ce364441183c9efb`);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=71655ad9b62c3bd8ce364441183c9efb`);
         setWeather(response.data)
         console.log(weather);
     };
